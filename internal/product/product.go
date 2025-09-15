56b21c3f90b68ac65673317d74c4d02ec4fcd899
arthur-psp <apspolti@gmail.com>
2025-09-15 20:05:57 -0300

    :recycle: ajusta nome das variaveis na CRUD de produtos

--- internal/product/product.go
+++ internal/product/product.go
@@ -1,14 +1,15 @@
 package product
 
-import "github.com/shopspring/decimal"
-
 type Produt struct {
-	Id             int32           `json:"id" db:"id"`
-	Name           string          `json:"name" db:"name"`
-	Description    string          `json:"description" db:"description"`
-	Id_bucha       int32           `json:"id_bucha" db:"id_bucha"`
-	Id_acionamento int32           `json:"id_acionamento" db:"id_acionamento"`
-	Id_base        int32           `json:"id_base" db:"id_base"`
-	Capacity       int64           `json:"capacidade" db:"capacidade"`
-	Value          decimal.Decimal `json:"valor" db:"valor"`
+	Id                 int32  `json:"id" db:"id"`
+	Codigo             string `json:"codigo" db:"codigo"`
+	Description        string `json:"description" db:"description"`
+	CapacidadeEstatica int64  `json:"capacidade_estatica" db:"capacidade_estatica"`
+	CapacidadeTrabalho int64  `json:"capacidade_trabalho" db:"capacidade_trabalho"`
+	Reducao            string `json:"reducao" db:"reducao"`
+	AlturaBucha        int64  `json:"altura_bucha" db:"altura_bucha"`
+	Curso              int64  `json:"curso" db:"curso"`
+	Id_bucha           int32  `json:"id_bucha" db:"id_bucha"`
+	Id_acionamento     int32  `json:"id_acionamento" db:"id_acionamento"`
+	Id_base            int32  `json:"id_base" db:"id_base"`
 }
