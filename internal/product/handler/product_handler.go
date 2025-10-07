7cd051d7a23b32d4208e20feb562e0d9c1b474c6
arthur-psp <apspolti@gmail.com>
2025-10-06 21:27:19 -0300

    :hammer: refactor: corrige uso do paginate

--- internal/product/handler/product_handler.go
+++ internal/product/handler/product_handler.go
@@ -15,7 +15,7 @@ type ProductHandler struct {
 	productUseCase usecase.ProductUseCase
 }
 
-func NewProductHandler(productUseCase usecase.ProductUseCase) *ProductHandler{
+func NewProductHandler(productUseCase usecase.ProductUseCase) *ProductHandler {
 	return &ProductHandler{
 		productUseCase: productUseCase,
 	}
@@ -28,7 +28,7 @@ func (handler *ProductHandler) Create(response http.ResponseWriter, request *htt
 		http.Error(response, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-	
+
 	err = handler.productUseCase.Create(&product)
 	if err != nil {
 		http.Error(response, "Erro ao adicionar novo produto.", http.StatusInternalServerError)
@@ -41,7 +41,7 @@ func (handler *ProductHandler) Create(response http.ResponseWriter, request *htt
 	err = json.NewEncoder(response).Encode(&product)
 	if err != nil {
 		http.Error(response, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return 
+		return
 	}
 }
 
@@ -62,52 +62,35 @@ func (handler *ProductHandler) Fetch(response http.ResponseWriter, request *http
 }
 
 func (handler *ProductHandler) WithParams(w http.ResponseWriter, r *http.Request) {
-	buchaType := r.URL.Query().Get("bucha")
-	acionamentoType := r.URL.Query().Get("acionamento")
-	baseType := r.URL.Query().Get("base")
-
-	//paginate.SetDebugMode(true)
-
-	// if buchaType == "" {
-	// 	http.Error(w, "Parâmetros não recebidos", http.StatusInternalServerError)
-    //     return
-	// }
-	
-	// if acionamentoType == "" {
-	// 	http.Error(w, "Tipo do acionamento não recebido", http.StatusInternalServerError)
-    //     return
-	// }
-
 	params, err := paginate.BindQueryParamsToStruct(r.URL.Query())
 	if err != nil {
-        http.Error(w, "Invalid parameters", http.StatusBadRequest)
-        return
-    }
+		http.Error(w, "Invalid parameters", http.StatusBadRequest)
+		return
+	}
 
 	sql, args, err := paginate.NewBuilder().
-	Table("products p").
-	Model(&product.ProductWithComponents{}).
-	Select("p.*", "b.tipobucha AS tipo_bucha", "a.tipoacionamento AS tipo_do_acionamento", "bs.tipobase AS tipo_base").
-	LeftJoin("buchas b", "p.id_bucha = b.id").
-	LeftJoin("acionamentos a", "p.id_acionamento = a.id").
-	LeftJoin("bases bs", "p.id_base = bs.id").
-	Where("b.tipobucha LIKE ? AND a.tipoacionamento LIKE ? AND bs.tipobase LIKE ?", "%"+buchaType+"%", "%"+acionamentoType+"%", "%"+baseType+"%").
-	FromStruct(params).
-	BuildSQL()
+		Table("products p").
+		Model(&product.ProductWithComponents{}).
+		Select("p.*", "b.tipobucha AS tipo_bucha", "a.tipoacionamento AS tipo_do_acionamento", "bs.tipobase AS tipo_base").
+		LeftJoin("buchas b", "p.id_bucha = b.id").
+		LeftJoin("acionamentos a", "p.id_acionamento = a.id").
+		LeftJoin("bases bs", "p.id_base = bs.id").
+		FromStruct(params).
+		BuildSQL()
 
 	if err != nil {
-        http.Error(w, "Query build error", http.StatusInternalServerError)
-        return
+		http.Error(w, "Query build error", http.StatusInternalServerError)
+		return
 	}
 
 	rows, err := handler.productUseCase.WithParams(r.Context(), sql, args...)
 	if err != nil {
-        http.Error(w, "Não foi possível fitlrar por buchas do tipo quadrado.", http.StatusInternalServerError)
-        return
-    }
+		http.Error(w, "Não foi possível utlizar os filtros de pesquisa", http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-    err = json.NewEncoder(w).Encode(map[string]interface{}{
+	err = json.NewEncoder(w).Encode(map[string]interface{}{
 		"products_with_params": rows,
 		"total": len(rows),
 		"page": params.Page,
